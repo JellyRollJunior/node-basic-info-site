c7605cdf062281cbb70623212682df55ec883dba
Brandon Lin <brandonlin9716@gmail.com>
2025-02-09 15:41:36 +0800

    use routers

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
 const express = require("express");
 const app = express();
-const fs = require("fs/promises");
+const authorRouter = require("./routes/authorRouter.js");
 
-const getPath = (path) => {
-    if (path == "/") return "./index.html";
-    return `.${path}.html`;
-};
-
-app.get("/", async (req, res) => {
-    try {
-        const data = await fs.readFile(getPath(req.url), { encoding: "utf8" });
-        res.send(data);
-    } catch (error) {
-        const data = await fs.readFile("./404.html", { encoding: "utf8" });
-        res.send(data);
-    }
-});
-
-app.get("/about", async (req, res) => {
-    try {
-        const data = await fs.readFile(getPath(req.url), { encoding: "utf8" });
-        res.send(data);
-    } catch (error) {
-        const data = await fs.readFile("./404.html", { encoding: "utf8" });
-        res.send(data);
-    }
-});
-
-app.get("/contact-me", async (req, res) => {
-    try {
-        const data = await fs.readFile(getPath(req.url), { encoding: "utf8" });
-        res.send(data);
-    } catch (error) {
-        const data = await fs.readFile("./404.html", { encoding: "utf8" });
-        res.send(data);
-    }
-});
-
-app.get("*", async (req, res) => {
-    const data = await fs.readFile("./404.html", { encoding: "utf8" });
-    res.send(data);
-});
+app.use("/author", authorRouter);
+app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`My first express app listening on port: ${PORT}!`);
+    console.log(`My first Express app - listening on port ${PORT}!`);
 });
