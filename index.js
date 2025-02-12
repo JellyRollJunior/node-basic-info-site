f48d96377c12d3afdf0c67c5c3bd8dfd411cf858
Brandon Lin <brandonlin9716@gmail.com>
2025-02-12 10:42:21 +0800

    Install ejs

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const authorRouter = require("./routes/authorRouter.js");
 
 app.use("/author", authorRouter);
-app.get("/", (req, res) => res.send("Hello, world!"));
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`My first Express app - listening on port ${PORT}!`);
 });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
